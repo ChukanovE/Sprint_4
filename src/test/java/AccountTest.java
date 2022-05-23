53df957c74e9f7eadbb4cb05c589e40b08f3a899
ChukanovE <chukanov.evgeny@yandex.ru>
2022-05-23 17:40:19 +0400

    Подгрузка тестов 4 спринта

---
+++ src/test/java/AccountTest.java
@@ -0,0 +1,47 @@
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+@RunWith(Parameterized.class)
+public class AccountTest {
+    private String name;
+    private boolean isNameCorrect;
+
+    public AccountTest (String name, boolean isNameCorrect) {
+        this.name = name;
+        this.isNameCorrect = isNameCorrect;
+    }
+
+    @Parameterized.Parameters
+    public static Object [][] getNameAccount() {
+        return new Object[][] {
+                {"Тимоти Шаламе", true},
+                {"тимоти шаламе", true},
+                {"т ш", true},
+                {"Тимоти Шаламеаааааа", true}, // 19 символов
+                {"Тимоти Шаламеааааааа", false}, // 20 символов
+                {"Timoti Shakame", false}, // English
+                {"ТимотиШаламе", false},
+                {"Тимоти Петрович Шаламе", false},
+                {" Тимоти Шаламе ", false},
+                {" Тимоти Шаламе", false},
+                {"Тимоти Шаламе ", false},
+                {"ТШ", false},
+                {"", false},
+                {null, false}
+        };
+    }
+
+    @Test
+    @DisplayName("Проверка метода checkNameToEmboss")
+    public void checkNameToEmbossTest() {
+        try {
+            Account account = new Account(name);
+            Assert.assertEquals(name + ": не прошел проверку", isNameCorrect, account.checkNameToEmboss());
+        } catch (Exception exception) {
+            assert exception instanceof NullPointerException;
+        }
+    }
+}
